00fa0c90777974f0912aab2d73c42dfee6a8f8c5
Michael Moser <Michael.Moser11@web.de>
2024-10-30 16:42:33 +0100

    current parsing progress
    next step: parse the content that was read and fill t_input struct

---
+++ src/main.c
@@ -0,0 +1,61 @@
+
+#include "../cub3d.h"
+#include <stddef.h>
+
+void	skip_spaces(const char *str, size_t *idx)
+{
+	while (str[*idx] && str[*idx] == ' ')
+	{
+		(*idx)++;
+	}
+}
+
+int	parse_content(t_input *input, const char *content)
+{
+	bool	in_map;
+	size_t	i;
+
+	in_map = false;
+	i = 0;
+	while (content[i])
+	{
+		skip_spaces(content, &i);
+
+	}
+
+
+	return (0);
+}
+
+int	parse_file(t_input *input, const char *map_path)
+{
+	char	*file_content;
+
+	file_content = read_file(map_path);
+	if (!file_content)
+		return (-1);
+	if (parse_content(input, file_content) == -1)
+	{
+		free(file_content);
+		return (-1);
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_cub3d	cub3d;
+	t_input	input;
+
+	if (argc != 2)
+	{
+		put_err(USAGE);
+		return (1);
+	}
+	if (parse_file(&input, argv[1]) == -1)
+		return 1;
+
+
+
+	return (0);
+}
