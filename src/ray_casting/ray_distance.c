f5bd72a88a5f195a5102e37029db89e5b78b2776
Douwe De Boer <dode-boe@f0r3s24.codam.nl>
2024-11-27 21:03:14 +0100

    wrote alternative get_vec_len

--- src/ray_casting/ray_distance.c
+++ src/ray_casting/ray_distance.c
@@ -2,16 +2,28 @@
 #include "../../cub3d.h"
 #include <math.h>
 
-const double	get_vec_len(t_vec start, t_vec end)
+const double	get_vec_len(t_vec start, t_vec end, t_deg angle)
 {
 	double	cath1;
 	double	cath2;
 	double	hypoth;
+	double	alpha;
 
+	if ((uint32_t)angle % 90 < 5 || (uint32_t)angle % 90 > 85)
+	{
+		cath1 = abs((int32_t) (start.x - end.x));
+		cath2 = abs((int32_t) (start.y - end.y));
+		hypoth = sqrt(cath1 * cath1 + cath2 * cath2);
+		return (hypoth);
+	}
 	cath1 = abs((int32_t) (start.x - end.x));
-	cath2 = abs((int32_t) (start.y - end.y));
-	hypoth = sqrt(cath1 * cath1 + cath2 * cath2);
-	return (hypoth);
+	if ((angle > 90 && angle < 180) || angle > 270)
+		alpha = 90 - ((u_int16_t)(angle) % 90 + (angle - (uint16_t)angle));
+	else
+		alpha = (u_int16_t)(angle) % 90 + (angle - (uint16_t)angle);
+	return (cath1 / cos(deg_to_rad(alpha)));
+	//= abs(1/start.y-end.y)
+	//= abs(1/start.x-end.y)
 }
 
 const double	get_actual_distance(t_ray *ray, t_deg view_angle)
@@ -38,14 +50,14 @@ void	find_distance_to_wall(t_ray	*ray, t_cor_px pov, t_deg view_angle, t_map *ma
 	if (ray->angle != 0 && ray->angle != 180)
 	{
 		hor_intersec_hit = get_hor_intersec_hit(pov, ray->angle, map);
-		hor_hit_distance = get_vec_len(pov, hor_intersec_hit);
+		hor_hit_distance = get_vec_len(pov, hor_intersec_hit, ray->angle);
 	}
 
 	// use DDA to find first wall at vertical intersections
 	if (ray->angle != 90 && ray->angle != 270)
 	{
 		ver_intersec_hit = get_ver_intersec_hit(pov, ray->angle, map);
-		ver_hit_distance = get_vec_len(pov, ver_intersec_hit);
+		ver_hit_distance = get_vec_len(pov, ver_intersec_hit, ray->angle);
 	}
 
 	if ((ver_hit_distance < hor_hit_distance && ver_hit_distance > 0) || hor_hit_distance == -1)
