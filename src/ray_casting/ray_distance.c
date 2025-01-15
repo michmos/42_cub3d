c1d6a90e96ae686ffa86cb364f22e3724794ccd4
Michael Moser <Michael.Moser11@web.de>
2025-01-15 15:34:09 +0100

    Fix visual bug

--- src/ray_casting/ray_distance.c
+++ src/ray_casting/ray_distance.c
@@ -26,40 +26,43 @@ static u_int32_t	get_actual_distance(t_ray *ray, t_deg view_angle)
 
 void	find_distance_to_wall(t_ray	*ray, t_dvec pov, t_deg view_angle, t_map *map)
 {
-	t_dvec	hor_intersec_hit;
-	t_dvec	ver_intersec_hit;
-	double	hor_hit_distance;
-	double	ver_hit_distance;
+	t_ray	hor_hit;
+	t_ray	ver_hit;
+	static	bool	last_was_ver = false;
+	double	diff;
 
-	hor_hit_distance = -1;
-	ver_hit_distance = -1;
+	hor_hit.ray_distance = -1;
+	ver_hit.ray_distance = -1;
 
 	// use DDA to find first wall at horizontal intersections
 	if (ray->angle != 0 && ray->angle != 180)
 	{
-		hor_intersec_hit = get_hor_intersec_hit(pov, ray->angle, map);
-		hor_hit_distance = get_vec_len(pov, hor_intersec_hit);
+		hor_hit.intersec = get_hor_intersec_hit(pov, ray->angle, map);
+		hor_hit.ray_distance = get_vec_len(pov, hor_hit.intersec);
 	}
 	// use DDA to find first wall at vertical intersections
 	if (ray->angle != 90 && ray->angle != 270)
 	{
-		ver_intersec_hit = get_ver_intersec_hit(pov, ray->angle, map);
-		ver_hit_distance = get_vec_len(pov, ver_intersec_hit);
+		ver_hit.intersec = get_ver_intersec_hit(pov, ray->angle, map);
+		ver_hit.ray_distance = get_vec_len(pov, ver_hit.intersec);
 	}
 
-	if ((ver_hit_distance < hor_hit_distance && ver_hit_distance > 0) || hor_hit_distance == -1)
+	diff = fabs(hor_hit.ray_distance - ver_hit.ray_distance);
+	if ((diff < 0.3 && last_was_ver)
+	|| ((ver_hit.ray_distance < hor_hit.ray_distance && ver_hit.ray_distance > 0) && diff > 0.3) || hor_hit.ray_distance == -1)
 	{
-		ray->intersec = ver_intersec_hit;
+		ray->intersec = ver_hit.intersec;
+		ray->ray_distance = ver_hit.ray_distance;
 		ray->vrtcl_intersec = true;
-		ray->ray_distance = ver_hit_distance;
 	}
 	else
 	{
-		ray->intersec = hor_intersec_hit;
+		ray->intersec = hor_hit.intersec;
+		ray->ray_distance = hor_hit.ray_distance;
 		ray->vrtcl_intersec = false;
-		ray->ray_distance = hor_hit_distance;
 	}
+	last_was_ver = ray->vrtcl_intersec;
+
 	// correct ray distance for distortion (fish eye view)
 	ray->actual_distance = get_actual_distance(ray, view_angle);
 }
-
