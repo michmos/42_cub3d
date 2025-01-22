7b55ff89ee0919fbd3d80ea51c2da9d23ec13ae9
Michael Moser <Michael.Moser11@web.de>
2025-01-22 12:11:12 +0100

    cleanup header

--- cub3d.h
+++ cub3d.h
@@ -19,13 +19,13 @@
 # define USAGE_ERR "USAGE: ./cub3d <map>"
 # define BLOCK_SIZE_ERR "BLOCK_SIZE needs to be a power of two for performance reasons"
 
-
 # define READ_BUF_SIZE 100							// size of chunk of bytes to read config file
 # define LOG2_BLOCKS_SIZE ((int) log2(BLOCK_SIZE))	// used for bit shifting when div or mult
 
 typedef char t_error;
 typedef double t_rad;
 typedef double t_deg;
+
 typedef enum e_fields
 {
 	VOID = ' ',
@@ -61,30 +61,12 @@ typedef union u_rgb
 	int	rgb;
 } t_rgb;
 
-typedef struct s_in_stream
-{
-	char	*buffer;
-	size_t	idx;			// to go through the file
-	size_t	crnt_lne_nbr;	// for error messages
-} t_in_stream;
-
 typedef struct s_map
 {
 	char	*map;
 	size_t	width;
 	size_t	height;
 } t_map;
-
-typedef struct s_input
-{
-	t_map	map;
-	char	*north_txtre;
-	char	*south_txtre;
-	char	*west_txtre;
-	char	*east_txtre;
-	t_rgb	floor;
-	t_rgb	ceiling;
-}	t_input;
 
 typedef struct s_dvec
 {
@@ -101,30 +83,10 @@ typedef struct s_vec
 // coordinates in blocks
 typedef t_vec t_cor_bl;
 
-// data per ray
-typedef struct s_ray
-{
-	t_deg		angle;				// angle in deg
-	t_dvec	intersec;			// coordinates of intersection with wall block
-	bool		vrtcl_intersec;		// type of intersec
-	double		ray_distance;		// distance to wall intersec
-	uint32_t	actual_distance;	// undistorted distance
-} t_ray;
-
-// data per vertical
-typedef struct s_wall_data
-{
-	u_int16_t	upper_end;			// upper y coordinate of the wall in pxl
-	u_int16_t	scld_height;		// scaled height of the wall in pxl
-	u_int16_t	txtre_x_pos;		// offset in texture
-	u_int16_t	distance;			// distance to wall
-	mlx_image_t	*img;				// wall image (E, N, W or S)
-} t_wall_data;
-
 typedef struct	s_view
 {
-	t_deg		dir_angle;		// current orientation of player (direction)
-	size_t		plane_distance; // distance between player and plane
+	t_deg	dir_angle;		// current orientation of player (direction)
+	size_t	plane_distance; // distance between player and plane
 } t_view;
 
 typedef struct	s_walls
@@ -145,70 +107,40 @@ typedef struct s_cub_mmap
 
 typedef struct s_cub3d
 {
-	mlx_t		*mlx;			// mlx window
+	mlx_t		*mlx;		// mlx window
 	mlx_image_t	*cur_frame;
 	mlx_image_t	*nxt_frame;
-	t_walls		wall_imgs;		// wall textures
-	t_rgb		floor;			// floor color
-	t_rgb		ceiling;		// ceiling color
-	t_map		map;			// map data
-	t_view		view;			// viewing field
-	t_dvec		player_pos;		// player position in pxl
+	t_walls		wall_imgs;	// wall textures
+	t_rgb		floor;		// floor color
+	t_rgb		ceiling;	// ceiling color
+	t_map		map;		// map data
+	t_view		view;		// viewing field
+	t_dvec		player_pos;	// player position in pxl
 	t_cub_mmap	mini;
 }	t_cub3d;
-
-typedef enum e_screen_dims
-{
-	SCREEN_WIDTH = 3840,
-	SCREEN_HEIGHT = 2160,
-	IMAGE_WIDTH = SCREEN_WIDTH / 2,
-	IMAGE_HEIGHT = SCREEN_HEIGHT / 2,
-	MINIMAP_MAX_WIDTH = IMAGE_WIDTH / 4,
-	MINIMAP_MAX_HEIGHT = IMAGE_HEIGHT / 4
-}	t_screen_dims;
-
-typedef struct s_minimap_dims
-{
-	uint16_t	square;
-	uint16_t	height;
-	uint16_t	width;
-}	t_minimap_dims;
-
-typedef struct s_minimap_help
-{
-	size_t			y;
-	size_t			x;
-	mlx_image_t	*img;
-	mlx_t		*mlx;
-}		t_minimap_help;
-
-typedef enum e_minimap_colours
-{
-	VOID_COLOUR = 0x010000,
-	SPACE_COLOUR = 0xF73E3E,
-	WALL_COLOUR = 0x3EF776,
-	PLAYER_COLOUR = 0xFFFFFF 
-}	t_minimap_colours;
-
-typedef enum e_player
-{
-	MODE_X,
-	MODE_Y
-}	t_player;
-
-typedef enum e_movedata
-{
-	FORWARD = 2,
-	BACKWARD = 3,
-	LEFT = 4,
-	RIGHT = 5,
-	COUNTER_CLOCKWISE = 1,
-	CLOCKWISE = -1
-}	t_movedata;
 
 ////////////////////////////////////////////////////////////////////////////////
 // parsing																	  //
 ////////////////////////////////////////////////////////////////////////////////
+
+typedef struct s_in_stream
+{
+	char	*buffer;
+	size_t	idx;			// to go through the file
+	size_t	crnt_lne_nbr;	// for error messages
+} t_in_stream;
+
+typedef struct s_input
+{
+	t_map	map;
+	char	*north_txtre;
+	char	*south_txtre;
+	char	*west_txtre;
+	char	*east_txtre;
+	t_rgb	floor;
+	t_rgb	ceiling;
+}	t_input;
+
 
 // read_file.c =------------------------------------------------------------- //
 char	*read_file(int	fd);
@@ -250,21 +182,43 @@ void	replace_spaces(char *str);
 // ray_casting																  //
 ////////////////////////////////////////////////////////////////////////////////
 
+// data per ray
+typedef struct s_ray
+{
+	t_deg		angle;				// angle in deg
+	t_dvec		intersec;			// coordinates of intersection with wall block
+	bool		vrtcl_intersec;		// type of intersec
+	double		ray_distance;		// distance to wall intersec
+	uint32_t	actual_distance;	// undistorted distance
+} t_ray;
+
+// data per vertical
+typedef struct s_wall_data
+{
+	u_int16_t	upper_end;		// upper y coordinate of the wall in pxl
+	u_int16_t	scld_height;	// scaled height of the wall in pxl
+	u_int16_t	txtre_x_pos;	// offset in texture
+	u_int16_t	distance;		// distance to wall
+	mlx_image_t	*img;			// wall image (E, N, W or S)
+} t_wall_data;
+
 // init_cub3d.c ------------------------------------------------------------- //
-t_error		init_cub3d(t_cub3d *cub3d, t_input *input);
+t_error	init_cub3d(t_cub3d *cub3d, t_input *input);
 
 // intersec_hit.c ----------------------------------------------------------- //
 t_dvec	get_ver_intersec_hit(t_dvec pov, t_deg ray_angle, t_map *map);
 t_dvec	get_hor_intersec_hit(t_dvec pov, t_deg ray_angle, t_map *map);
 
 // ray_distance.c ----------------------------------------------------------- //
-void		find_distance_to_wall(t_ray	*ray, t_dvec pov, t_deg view_angle, t_map *map);
+void	find_distance_to_wall(t_ray	*ray, t_dvec pov, \
+							t_deg view_angle, t_map *map);
 
 // draw_view.c -------------------------------------------------------------- //
-t_error draw_view(t_cub3d *cub3d);
+t_error	draw_view(t_cub3d *cub3d);
 
 // draw_vertical.c ---------------------------------------------------------- //
-void	draw_vertical(t_cub3d *cub3d, u_int16_t frame_x_pos, u_int16_t scaled_height, t_ray *ray);
+void	draw_vertical(t_cub3d *cub3d, u_int16_t frame_x_pos, \
+				   u_int16_t scaled_height, t_ray *ray);
 
 // conversions.c ------------------------------------------------------------ //
 double		rad_to_deg(t_rad radians);
@@ -295,13 +249,55 @@ void	free_input(t_input *input);
 // movement																	  //
 ////////////////////////////////////////////////////////////////////////////////
 
+typedef enum e_movedata
+{
+	FORWARD = 2,
+	BACKWARD = 3,
+	LEFT = 4,
+	RIGHT = 5,
+	COUNTER_CLOCKWISE = 1,
+	CLOCKWISE = -1
+}	t_movedata;
+
 u_int8_t	move(t_cub3d *cub, t_movedata dir);
 u_int8_t	rotate(t_cub3d *cub, t_movedata dir);
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // minimap																	  //
 ////////////////////////////////////////////////////////////////////////////////
+
+typedef enum e_screen_dims
+{
+	SCREEN_WIDTH = 3840,
+	SCREEN_HEIGHT = 2160,
+	IMAGE_WIDTH = SCREEN_WIDTH / 2,
+	IMAGE_HEIGHT = SCREEN_HEIGHT / 2,
+	MINIMAP_MAX_WIDTH = IMAGE_WIDTH / 4,
+	MINIMAP_MAX_HEIGHT = IMAGE_HEIGHT / 4
+}	t_screen_dims;
+
+typedef struct s_minimap_dims
+{
+	uint16_t	square;
+	uint16_t	height;
+	uint16_t	width;
+}	t_minimap_dims;
+
+typedef struct s_minimap_help
+{
+	size_t			y;
+	size_t			x;
+	mlx_image_t		*img;
+	mlx_t			*mlx;
+}	t_minimap_help;
+
+typedef enum e_minimap_colours
+{
+	VOID_COLOUR = 0x010000,
+	SPACE_COLOUR = 0xF73E3E,
+	WALL_COLOUR = 0x3EF776,
+	PLAYER_COLOUR = 0xFFFFFF 
+}	t_minimap_colours;
 
 t_error	draw_minimap(t_cub3d *cub);
 bool	is_player_pos(char c);
